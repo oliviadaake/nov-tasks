24a4f9acdd798eb0289c44a2cd90b571947db686
oliviadaake <123674294+oliviadaake@users.noreply.github.com>
2023-11-29 11:33:04 -0600

    Test
    test

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
+
+export const routes: Routes = [{
+
+    path: 'tasks',
+    component: TaskListComponent,
+
+}];
